7d82d5a294d8c56df84b12e299750b1af7baf538
Scott Ritchie <sritchie73@gmail.com>
2017-03-21 14:50:19 +1100

    Bugfix to check_range
    Now prints out message for filtered samples.

--- R/check_function.R
+++ R/check_function.R
@@ -20,17 +20,17 @@ check_range <- function(measurement, name) {
     measurement[measurement > measurement_ranges[name, "max_val"]] <- NA
   }
   after_n <- sum(!is.na(measurement))
-  filtered_n <- after_n - before_n
+  filtered_n <- before_n - after_n
   if (filtered_n > 0) {
     if (name == "Sex") {
       warning(filtered_n, " samples with unrecognisable sex coding (Male == 1, Female == 2)",
               " removed")
     } else if (name %in% c("AGP", "A1AT", "HP", "TF")) {
-      message(filtered_n, " imputed ", name, " measurements outside of acceptable range (",
+      message(filtered_n, " imputed \"", name, "\" measurements outside of acceptable range (",
               measurement_ranges[name, "min_val"], "--", measurement_ranges[name, "max_val"],
               " ", measurement_ranges[name, "units"], ") set to 'NA'")
     } else {
-      message(filtered_n, " ", name, " measurements outside of acceptable range (",
+      message(filtered_n, " \"", name, "\" measurements outside of acceptable range (",
               measurement_ranges[name, "min_val"], "--", measurement_ranges[name, "max_val"],
               " ", measurement_ranges[name, "units"], ") set to 'NA'")
     }
